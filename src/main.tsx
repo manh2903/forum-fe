33c06519dcf3efa994da497a4843b19d0ecf450e
manh2903 <manhdz22pl@gmail.com>
2026-03-23 13:57:11 +0700

    bỏ dev mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import { Toaster } from 'react-hot-toast'
-import App from './App'
-import { AuthProvider } from './contexts/AuthContext'
-import { theme } from './theme'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Toaster } from "react-hot-toast";
+import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+import { theme } from "./theme";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <AuthProvider>
-          <App />
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              style: {
-                background: '#ffffff',
-                color: '#0f172a',
-                border: '1px solid #e2e8f0',
-              },
-            }}
-          />
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "#ffffff",
+              color: "#0f172a",
+              border: "1px solid #e2e8f0",
+            },
+          }}
+        />
+      </AuthProvider>
+    </ThemeProvider>
+  </BrowserRouter>,
+);
